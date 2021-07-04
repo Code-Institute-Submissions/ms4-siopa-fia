0891a43b5d604badc0ce3fca15fa5781e3765396
elerel <elcranley@gmail.com>
2021-07-04 15:12:50 +0000

    feat: added faqs and contact to main navbar

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Blog(models.Model):
     title = models.CharField(max_length=200, unique=True)
     body = models.TextField()
+    
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     published_date = models.DateTimeField(auto_now_add=True)
 
