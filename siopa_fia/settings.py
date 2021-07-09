8b404b32dfec19c3b5e10ff0210e535747ac8671
elerel <elcranley@gmail.com>
2021-07-09 11:58:55 +0000

    feat: removed secret key and set debug

--- siopa_fia/settings.py
+++ siopa_fia/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hsew_$0a0rc!empqs@ss^mr3b++t2!zzk%vu(&ft4-p1eiu+*h'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['https://siopafia.herokuapp.com/', 'localhost']
 
