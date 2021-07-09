53cd6efe5703edbe430b2b7e33037278985bcd3a
elerel <elcranley@gmail.com>
2021-07-09 12:33:01 +0000

    fix: modified allowed host details to open app issue

--- siopa_fia/settings.py
+++ siopa_fia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://siopafia.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['siopafia.herokuapp.com', 'localhost']
 
 
 # Application definition
