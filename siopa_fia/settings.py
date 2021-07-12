78fa5a49d635149463e112b5b514080fc3fd0e10
elerel <elcranley@gmail.com>
2021-07-12 11:34:55 +0000

    debug: set to True

--- siopa_fia/settings.py
+++ siopa_fia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['siopafia.herokuapp.com', 'localhost']
 
